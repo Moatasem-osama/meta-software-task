f43465596847114c459b170721049ab90361a590
Moatasem Osama <asomyosamalahawary@gmail.com>
2026-01-28 18:15:55 +0200

    add sorting and filtration

--- app/Table.tsx
+++ app/Table.tsx
@@ -1,11 +1,59 @@
 "use client";
+
+import { useState  } from "react";
 import assets from "../public/assets/assets.json";
 
 export default function Table() {
-  
+const [sortBy, setSortBy] = useState("none");
+  const [filter, setFilter] = useState("ALL");
+  const filteredAssets =
+    filter === "ALL" ? assets : assets.filter((a) => a.type === filter);
 
+  
+const sortedAssets = [...filteredAssets];
+if (sortBy === "price-asc") {
+  sortedAssets.sort((a, b) => a.currentPrice - b.currentPrice);
+} else if (sortBy === "price-desc") {
+  sortedAssets.sort((a, b) => b.currentPrice - a.currentPrice);
+} else if (sortBy === "name-asc") {
+  sortedAssets.sort((a, b) => a.name.localeCompare(b.name));
+} else if (sortBy === "name-desc") {
+  sortedAssets.sort((a, b) => b.name.localeCompare(a.name));
+}
   return (
     <div className="p-6">
+      
+      <div className="my-5">
+        <label className="mr-2 font-semibold">Filter by Type:</label>
+
+        <select
+          name="dropdown"
+          id="filtration"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          className="border border-gray-300 rounded px-3 py-2"
+        >
+          <option value="ALL">ALL</option>
+          <option value="Stock">Stock</option>
+          <option value="Crypto">Crypto</option>
+          <option value="Mutual Fund">Mutual Fund</option>
+        </select>
+        <div className="my-5">
+          <label className="mx-4 font-semibold">Sort by:</label>
+        <select
+          name="sort"
+          id="sorting"
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+          className="border border-gray-300 rounded px-3 py-2"
+        >
+          <option value="price-asc">price-asc</option>
+          <option value="price-desc">price-desc</option>
+          <option value="name-asc">name-asc</option>
+          <option value="name-desc">name-desc</option>
+        </select>
+        </div>
+      </div>
       <h1 className="text-2xl font-bold mb-6">Investment assets</h1>
 
       <div className="p-10 relative overflow-x-auto shadow-sm rounded-md border border-gray-200">
@@ -22,7 +70,7 @@ export default function Table() {
           </thead>
 
           <tbody>
-            {assets.map((asset, index) => {
+            {sortedAssets.map((asset, index) => {
               const profit =
                 (asset.currentPrice - asset.buyPrice) * asset.amount;
               return (
