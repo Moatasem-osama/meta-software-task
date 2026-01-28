4fbbd679253ed10dd15ddf75e3fa6c754e44314c
Moatasem <asomyosamaalhawary@gmail.com>
2026-01-28 23:44:54 +0200

    Add TypeScript types to Charts component
    Refactor Charts component to use TypeScript types for props and improve code readability.

--- app/Charts.tsx
+++ app/Charts.tsx
@@ -3,41 +3,47 @@
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Tooltip, Legend } from "chart.js";
 
-
 ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
-export default function Charts({data}) {
-  const labels = data.map(item => item.name);
-
-  const profits = data.map(item => {
-    return (item.currentPrice - item.buyPrice) * item.amount;
-  });
-
-  const backgroundColors = profits.map(value =>
-  value >= 0 ? "rgba(34,197,94,0.7)" : "rgba(239,68,68,0.7)"
-);
-
-const allData = {
-  labels,
-  datasets: [
-    {
-      label: "Profit / Loss ($)",
-      data: profits,
-      backgroundColor: backgroundColors,
-      borderRadius: 6,
-    },
-  ],
+// نحدد نوع كل عنصر في البيانات
+type AssetItem = {
+  name: string;
+  amount: number;
+  buyPrice: number;
+  currentPrice: number;
 };
 
+type ChartsProps = {
+  data: AssetItem[];
+};
 
+export default function Charts({ data }: ChartsProps) {
+  const labels = data.map(item => item.name);
+
+  const profits = data.map(item => (item.currentPrice - item.buyPrice) * item.amount);
+
+  const backgroundColors = profits.map(value =>
+    value >= 0 ? "rgba(34,197,94,0.7)" : "rgba(239,68,68,0.7)"
+  );
+
+  const allData = {
+    labels,
+    datasets: [
+      {
+        label: "Profit / Loss ($)",
+        data: profits,
+        backgroundColor: backgroundColors,
+        borderRadius: 6,
+      },
+    ],
+  };
 
   return (
-  <div className="rounded-xl w-full shadow-md p-6 border">
-  <h2 className="text-lg font-semibold mb-4 text-gray-800">
-    Portfolio Profit / Loss
-  </h2>
-  <Bar data={allData}  />
-</div>
-
+    <div className="rounded-xl w-full shadow-md p-6 border">
+      <h2 className="text-lg font-semibold mb-4 text-gray-800">
+        Portfolio Profit / Loss
+      </h2>
+      <Bar data={allData} />
+    </div>
   );
 }
